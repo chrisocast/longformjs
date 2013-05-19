00da6db8ce7767be657edc743e74d8cd3b67ad72
Chris Cast <chrisocast@gmail.com>
2013-05-19 13:01:04 -0700

    Refactor for poster component

--- js/src/templates/templates.js
+++ js/src/templates/templates.js
@@ -8,7 +8,7 @@ helpers = helpers || Handlebars.helpers; data = data || {};
   var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  buffer += "<section class=\"media\">\n  <img src=\"";
+  buffer += "<section class=\"media poster\">\n  <img src=\"";
   if (stack1 = helpers.src) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.src; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
